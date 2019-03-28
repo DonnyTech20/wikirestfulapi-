b5f979f3c7f775233b9c633a52a30bd61c029d42
Donny Techiera <DonnyTech20@gmail.com>
2019-03-27 21:54:01 -0400

    started server

---
+++ app.js
@@ -0,0 +1,22 @@
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const mongoose = require("mongoose");
+
+const app = express();
+
+app.set("view engine", "ejs");
+
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+app.use(express.static("public"));
+
+
+
+
+app.listen(3000, () => {
+    console.log("Dev your Server is on port 5000");
+});
